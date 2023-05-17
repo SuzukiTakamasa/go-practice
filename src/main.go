c4152a904de324a0b7502ff0c12f98a951df30b3
SuzukiTakamasa <69415488+SuzukiTakamasa@users.noreply.github.com>
2023-05-17 23:56:39 +0900

    mod

--- src/main.go
+++ src/main.go
@@ -4,6 +4,22 @@ import (
 	"fmt"
 )
 
+type TestStruct struct {
+	hoge string
+	huga string
+}
+
+func (test *TestStruct) modify(i string, j string) {
+	test.hoge = i
+	test.huga = j
+}
+
 func main() {
-	fmt.Println("This is just a scenarigo practice.")
+	test_1 := TestStruct{
+		hoge: "a",
+		huga: "b",
+	}
+	fmt.Println(test_1)
+	test_1.modify("c", "d")
+	fmt.Println(test_1)
 }
